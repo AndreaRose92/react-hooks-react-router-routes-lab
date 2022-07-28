0087a3d33eb53876a6389f5d4280d9f80a438762
Andrea Rosentel <andrearosentel@gmail.com>
2022-07-28 13:05:35 -0700

    className navbar

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { movies } from "../data";
 
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const displayMovies = movies.map(movie => 
+  <div key={movie.time} >
+    <h1>{movie.title}</h1>
+    <h2>{movie.time}</h2>
+    <ul>{movie.genres.map(genre => <li key={Math.random()} >{genre}</li> )}</ul>
+  </div>
+  )
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {displayMovies}
+    </div>
+  )
 }
 
 export default Movies;
